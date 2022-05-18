4e82a0310ab8e8c9b74dc73bffd73d837cf72a06
dhoim <hoimdominik@gmail.com>
2022-05-18 17:35:27 +0200

    initial commit

---
+++ MAIN2/smakolyki.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+
+const int M = 1<<20;
+int tab[M], rep[M];
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n, m;
+    uint64_t res = 0;
+    cin >> n >> m;
+    
+    for(int i = 1; i <= n; i++){
+        cin >> tab[i];
+    }
+
+    for(int l = 1, r = 1; r <= n; r++){
+        rep[tab[r]]++;
+
+        while(l <= r && rep[tab[r]] > 1){
+            rep[tab[l]]--;
+            l++;
+        }
+
+        if(rep[tab[r]] < 2){
+            res += r - l + 1;
+        }
+    }
+
+    cout << res;
+    return 0;
+}
